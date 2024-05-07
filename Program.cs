4ec0c14461e00adc6b1581869b803e52790267ed
Alexander Pridava <sanyapridava@mail.ru>
2024-05-07 15:15:12 +0300

    Add work with files and handling exception(not exists file)

--- Program.cs
+++ Program.cs
@@ -8,16 +8,28 @@ public class Program {
 
         var convertToPN = new LeksemConvertToPN();
 
-        string s = "tg(x / 5 + cos(x - 5)) * (5 - x)";
-        string[] S = convertToPN.Polska(s);
-        
-        string result = "";
-        foreach (var str in S) {
-            result += str;
+        Console.WriteLine("Введите название теста: ");
+        string variant = Console.ReadLine();        
+
+        try {
+            //Read data from files of TestData
+            using (StreamReader program = new StreamReader($"C:\\translator\\test_env\\TestData\\{variant}")) {
+                string s = "";
+                string line;
+                while ((line = program.ReadLine()) != null) s += line;
+            
+                string[] S = convertToPN.Polska(s);
+                
+                string result = "";
+                foreach (var str in S) {
+                    result += str;
+                }
+                Console.WriteLine(result);
+
+
+            }
+        } catch (Exception e) {
+            Console.WriteLine("Данный файл не существует!");
         }
-        Console.WriteLine(result);
-
-
     }
-    
 }
